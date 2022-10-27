298411355668709cc9f1c03882974c29c7c0ee1b
Marian Zburlea <marian@wowdop.com>
2022-10-27 21:36:14 +0100

    ggg

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
   experimental: {
     // Required:
@@ -7,4 +7,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
